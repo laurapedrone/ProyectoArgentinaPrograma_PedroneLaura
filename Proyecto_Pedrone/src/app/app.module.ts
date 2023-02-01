5b5c9c6102e90e6e46748b8f02d6979446f279d5
laurapedrone <119712517+laurapedrone@users.noreply.github.com>
2023-02-01 10:23:54 -0300

    Realización del Header

--- Proyecto_Pedrone/src/app/app.module.ts
+++ Proyecto_Pedrone/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
