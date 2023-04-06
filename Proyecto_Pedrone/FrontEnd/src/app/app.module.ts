d612cd01d7428d54f861838307db69a1c175ce3c
Laura Pedrone <lau.pedrone@gmail.com>
2023-04-06 20:29:14 -0300

    Creacion de Login

--- Proyecto_Pedrone/FrontEnd/src/app/app.module.ts
+++ Proyecto_Pedrone/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { HomeComponent } from './components/home/home.component';
     HysComponent,
     ProyectoComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
