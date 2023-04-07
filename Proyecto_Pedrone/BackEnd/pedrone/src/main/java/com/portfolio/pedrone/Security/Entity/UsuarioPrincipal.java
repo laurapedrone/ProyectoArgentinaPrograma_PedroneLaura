30ce333e34f511c32c4c64c88e328f5fc1f39a0b
Laura Pedrone <lau.pedrone@gmail.com>
2023-04-07 19:04:26 -0300

    Rol y Usuarios

---
+++ Proyecto_Pedrone/BackEnd/pedrone/src/main/java/com/portfolio/pedrone/Security/Entity/UsuarioPrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.pedrone.Security.Entity;
+
+import java.util.Collection;
+
+
+public class UsuarioPrincipal implements UserDetails{
+    private String nombre;
+    private String nombreUsuario;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    
+    //Constructor
+    public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+    public static UsuarioPrincipal build(Usuario usuario){
+        
+    }
+    
+}
--- Proyecto_Pedrone/FrontEnd/src/app/app-routing.module.ts
+++ Proyecto_Pedrone/FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'login',component: LoginComponent} 
+  //Aplica el ruteo en la ruta principal y que vaya directamente al homeComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
