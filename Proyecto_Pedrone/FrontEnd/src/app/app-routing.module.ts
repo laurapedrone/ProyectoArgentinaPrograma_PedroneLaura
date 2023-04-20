208e2caeb7bde64d14e313a1374d81f6cf96b77e
Laura Pedrone <lau.pedrone@gmail.com>
2023-04-19 22:15:42 -0300

    Ultimos detalles de Experiencia

--- Proyecto_Pedrone/FrontEnd/src/app/app-routing.module.ts
+++ Proyecto_Pedrone/FrontEnd/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'login', component: LoginComponent}, 
   //Aplica el ruteo en la ruta principal y que vaya directamente al homeComponent
-  {path: 'nuevaexp', component: NewExperienciaComponent}
+  {path: 'nuevaexp', component: NewExperienciaComponent},
+  {path: 'editexp/:id', component: EditExperienciaComponent}
 ];
 
 @NgModule({
--- Proyecto_Pedrone/FrontEnd/src/app/app.module.ts
+++ Proyecto_Pedrone/FrontEnd/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 
 
@@ -37,7 +38,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
     FooterComponent,
     HomeComponent,
     LoginComponent,
-    NewExperienciaComponent
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
